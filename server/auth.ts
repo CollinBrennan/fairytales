ae184830ab52791a34773066af77d6feab755c06
Collin Brennan <collinbrennan8@gmail.com>
2025-01-16 12:57:43 -0500

    set up uploadthing and clean up routes

---
+++ server/auth.ts
@@ -0,0 +1,64 @@
+import Google, { type GoogleProfile } from '@auth/core/providers/google'
+import { DrizzleAdapter } from '@auth/drizzle-adapter'
+import db from '@db/drizzle'
+import {
+  accounts,
+  authenticators,
+  sessions,
+  users,
+  verificationTokens,
+} from '@db/schema/users'
+import { getAuthUser, initAuthConfig } from '@hono/auth-js'
+import type { MiddlewareHandler } from 'hono'
+import { HTTPException } from 'hono/http-exception'
+
+export const authConfig = initAuthConfig(() => ({
+  secret: process.env.AUTH_SECRET,
+  strategy: 'database',
+  adapter: DrizzleAdapter(db, {
+    usersTable: users,
+    accountsTable: accounts,
+    sessionsTable: sessions,
+    verificationTokensTable: verificationTokens,
+    authenticatorsTable: authenticators,
+  }),
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      profile(profile: GoogleProfile) {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          emailVerified: profile.email_verified,
+          image: profile.picture,
+          role: 'user',
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    session({ session, user }) {
+      session.user.role = user.role
+      return session
+    },
+  },
+}))
+
+export function verifyAdmin(): MiddlewareHandler {
+  return async (c, next) => {
+    const authUser = await getAuthUser(c)
+    const isAdmin = authUser?.session.user?.role === 'admin'
+
+    if (!isAdmin) {
+      const res = new Response('Unauthorized', {
+        status: 401,
+      })
+      throw new HTTPException(401, { res })
+    }
+
+    c.set('authUser', authUser)
+    await next()
+  }
+}
--- server/routes/admin/admin-titles.ts
+++
@@ -1,3 +0,0 @@
-import { Hono } from 'hono'
-
-export const adminTitleRoute = new Hono().get('/', (c) => c.text('Hello!!'))
